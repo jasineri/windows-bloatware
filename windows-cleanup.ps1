34628d82fbe26b18aa0d48a75ac1266caa944b14
jasineri <jasineri@gmx.de>
2022-12-28 20:12:04 +0100

    windows-cleanup.ps1

--- windows-cleanup.ps1
+++ windows-cleanup.ps1
@@ -1,2 +1,4 @@
 Get-AppxPackage -AllUsers Microsoft.XboxGamingOverlay | Remove-AppxPackage
 Get-AppxPackage -AllUsers Microsoft.WindowsMaps | Remove-AppxPackage
+Get-AppxPackage -AllUsers Microsoft.GetHelp | Remove-AppxPackage
+Get-AppxPackage -AllUsers Microsoft.Windows.Photos | Remove-AppxPackage
